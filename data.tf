408491c05ac73e23bdf424f21cb8246feabd593a
mybofagit <mkums@hotmail.com>
2025-11-19 14:21:39 +0000

    This is myfirst commit

---
+++ data.tf
@@ -0,0 +1,4 @@
+data "aws_vpc" "example3_ds"{
+    # VPC id from AWS dashboard or VPC console.
+    id="vpc-07eede624774fec02"
+}
---
+++ securitygroups.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "example3_sg" {
+  name        = "kums_allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_vpc.example3_ds.id
+
+  tags = {
+    Name = "kums_filewall_rule"
+  }
+}
+
+# Allow all outgoing ports
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.example3_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_80_traffic_ipv4" {
+  security_group_id = aws_security_group.example3_sg.id
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+
+
+  }
+resource "aws_vpc_security_group_ingress_rule" "allow_22_traffic_ipv4" {
+  security_group_id = aws_security_group.example3_sg.id
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+
+
+  }
+
+
